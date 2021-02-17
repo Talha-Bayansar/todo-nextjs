732b65d3ce13043053d942c0fc00a70cbed0877e
Talha-Bayansar <talha.bayansar@hotmail.com>
2021-02-17 18:49:43 +0100

    changed padding navitems

--- components/Layout.js
+++ components/Layout.js
@@ -11,18 +11,20 @@ export const Layout = ({ children }) => {
         <div className="flex flex-col items-center w-full">
             <nav className="flex bg-gray-200 justify-between items-center fixed top-0 w-full h-16 px-8 z-10 shadow-lg">
                 <h1 className="text-3xl font-bold hidden sm:block">Logo</h1>
-                <div className="flex justify-evenly w-full sm:w-auto">
+                <div className="flex justify-evenly h-full w-full sm:w-auto">
                     <Link href="/">
-                        <a className="hover:bg-gray-300 active:bg-gray-300 px-4 py-2 rounded-lg">
+                        <a className="flex items-center hover:bg-gray-300 active:bg-gray-300 px-4 rounded-lg">
                             Startpagina
                         </a>
                     </Link>
                     <Link href="/tasks">
-                        <a className="hover:bg-gray-300 active:bg-gray-300 px-4 py-2 rounded-lg">
+                        <a className="flex items-center hover:bg-gray-300 active:bg-gray-300 px-4 rounded-lg">
                             Taken
                         </a>
                     </Link>
-                    <span className="m-2 px-4 py-2 text-green-700">{date}</span>
+                    <span className="flex items-center px-4 text-green-700">
+                        {date}
+                    </span>
                 </div>
             </nav>
             <div className="mt-16 w-full p-8">{children}</div>

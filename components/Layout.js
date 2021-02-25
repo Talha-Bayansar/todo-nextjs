0cd81c658bca7dfa8db13e3c0f7998c16a6c8e43
Talha-Bayansar <talha.bayansar@hotmail.com>
2021-02-25 17:35:42 +0100

    bottombar with logo on navbar done

--- components/Layout.js
+++ components/Layout.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 import HomeIcon from "@material-ui/icons/Home";
 import AssignmentTurnedInIcon from "@material-ui/icons/AssignmentTurnedIn";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import Image from "next/image";
 
 export const Layout = ({ children }) => {
     const currentDate = new Date();
@@ -15,10 +16,16 @@ export const Layout = ({ children }) => {
         .toString()
         .padStart(2, "0")}-${year.toString().padStart(2, "0")}`;
     return (
-        <div className="flex flex-col items-center w-full">
-            <nav className="flex bg-gray-200 justify-between items-center fixed top-0 w-full h-16 px-8 z-10 shadow-lg">
-                <h1 className="text-3xl font-bold hidden sm:block">Logo</h1>
-                <div className="flex justify-evenly w-full sm:w-auto">
+        <div className="flex flex-col items-center w-full h-screen sm:h-auto">
+            <nav className="flex bg-white justify-between items-center sm:fixed top-0 w-full h-16 py-4 px-8 z-10 shadow-lg">
+                {/* <h1 className="text-3xl font-bold block">Logo</h1> */}
+                <Image
+                    src="/icon.png"
+                    alt="Planner Logo"
+                    width={50}
+                    height={50}
+                />
+                <div className="hidden sm:flex sm:justify-evenly sm:flex-grow sm:w-auto">
                     <Link href="/">
                         <a className="flex items-center hover:bg-gray-300 active:bg-gray-300 p-4 rounded-full">
                             <HomeIcon />
@@ -34,12 +41,36 @@ export const Layout = ({ children }) => {
                             <ExitToAppIcon />
                         </a>
                     </Link>
-                    <span className="flex items-center px-4 text-blue-600 font-bold">
-                        {date}
-                    </span>
                 </div>
+                <span className="flex items-center text-blue-600 font-bold">
+                    {date}
+                </span>
             </nav>
-            <div className="mt-16 w-full p-8">{children}</div>
+
+            <div className="flex-grow overflow-y-scroll sm:mt-16 w-full p-8">
+                {children}
+            </div>
+
+            <nav
+                className="flex w-screen justify-evenly items-center h-16 py-4 px-8 z-10 shadow-reverse
+             sm:hidden bg-white"
+            >
+                <Link href="/">
+                    <a className="flex items-center hover:bg-gray-300 active:bg-gray-300 p-4 rounded-full">
+                        <HomeIcon />
+                    </a>
+                </Link>
+                <Link href="/tasks">
+                    <a className="flex items-center hover:bg-gray-300 active:bg-gray-300 p-4 rounded-full">
+                        <AssignmentTurnedInIcon />
+                    </a>
+                </Link>
+                <Link href="/tasks">
+                    <a className="flex items-center hover:bg-gray-300 active:bg-gray-300 p-4 rounded-full">
+                        <ExitToAppIcon />
+                    </a>
+                </Link>
+            </nav>
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -32,6 +32,8 @@ module.exports = {
             none: "none",
             mat:
                 "0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);",
+            reverse:
+                "0 -10px 15px -3px rgba(0, 0, 0, 0.1), 0 -4px 6px -2px rgba(0, 0, 0, 0.04)",
         },
         screens: {
             sm: "640px",

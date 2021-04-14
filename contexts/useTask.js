e22331b21fa646f98ca4821b911732f24dc4b39a
Talha-Bayansar <talha.bayansar@hotmail.com>
2021-04-14 03:46:34 +0200

    mutate when checking todo

--- contexts/useTask.js
+++ contexts/useTask.js
@@ -6,25 +6,6 @@ export function TaskProvider(props) {
     const [taskToEdit, setTaskToEdit] = useState({});
     const [taskToDelete, setTaskToDelete] = useState({});
     const [isDelete, setIsDelete] = useState(false);
-    const [allTasks, setAllTasks] = useState([]);
-
-    function addTask(task) {
-        setAllTasks([...allTasks, task]);
-    }
-
-    function deleteTask(task) {
-        setAllTasks(allTasks.filter((t) => t.id !== task.id));
-    }
-
-    function updateTask(task) {
-        let newArray = allTasks;
-        for (const i in newArray) {
-            if (newArray[i].id === task.id) {
-                newArray[i] = task;
-            }
-        }
-        setAllTasks(newArray);
-    }
 
     const api = {
         taskToEdit,
@@ -33,11 +14,6 @@ export function TaskProvider(props) {
         setIsDelete,
         taskToDelete,
         setTaskToDelete,
-        allTasks,
-        setAllTasks,
-        addTask,
-        deleteTask,
-        updateTask,
     };
 
     return (
